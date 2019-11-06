c47e7e60a18ca0c2e66d119034aa04c318ff7044
Damian Dlugosch <damian.dlugosch@pelicantravel.local>
2019-11-06 12:06:51 +0100

    Try to hide link in left bottom corner

---
+++ otazka4.php
@@ -0,0 +1,38 @@
+<?php include "head.html" ?>
+<?php include "header.html" ?>
+<main>
+    <div class="container">
+        <section class="form col-lg-12">
+            <div class="row">
+                <h1 class="col-lg-12 text-center py-3">Mám chuť na</h1>
+            </div>
+            <div class="row form--links py-4">
+                <a href="javascript:void(0)" onclick="window.location.href='otazka4.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/kava.png" alt="Káva">
+                    </figure>
+                    <h2 class="text-center">Káve</h2>
+                </a>
+                <a href="javascript:void(0)" onclick="window.location.href='otazka4.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/vino.png" alt="Víno">
+                    </figure>
+                    <h2 class="text-center">Víne</h2>
+                </a>
+                <a href="" onclick="window.location.href='otazka4.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/pivo.png" alt="Pivo">
+                    </figure>
+                    <h2 class="text-center">Pive</h2>
+                </a>
+                <a href="javascript:void(0)" onclick="window.location.href='otazka4.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/cocktail.png" alt="Koktail">
+                    </figure>
+                    <h2 class="text-center">Koktaily</h2>
+                </a>
+            </div>
+        </section>
+    </div>
+</main>
+<?php include "footer.html"?>
