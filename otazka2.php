9af4e7352b97eaa8613218469e7d3a5a4d53c550
Damian Dlugosch <damian.dlugosch@pelicantravel.local>
2019-11-06 11:43:30 +0100

    Initial commit

---
+++ otazka2.php
@@ -0,0 +1,38 @@
+<?php include "head.html" ?>
+<?php include "header.html" ?>
+<main>
+    <div class="container">
+        <section class="form col-lg-12">
+            <div class="row">
+                <h1 class="col-lg-12 text-center py-3">Ak by som bol zviera bol by som</h1>
+            </div>
+            <div class="row form--links py-4">
+                <a href="javascript:void(0)" onclick="window.location.href='otazka3.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/delfin.png" alt="Delfín">
+                    </figure>
+                    <h2 class="text-center">Delfín</h2>
+                </a>
+                <a href="javascript:void(0)" onclick="window.location.href='otazka3.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/opica.png" alt="Opica">
+                    </figure>
+                    <h2 class="text-center">Opica</h2>
+                </a>
+                <a href="javascript:void(0)" onclick="window.location.href='otazka3.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/maco.png" alt="Grizly">
+                    </figure>
+                    <h2 class="text-center">Grizly</h2>
+                </a>
+                <a href="javascript:void(0)" onclick="window.location.href='otazka3.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/pigon.png" alt="Holub">
+                    </figure>
+                    <h2 class="text-center">Holub</h2>
+                </a>
+            </div>
+        </section>
+    </div>
+</main>
+<?php include "footer.html"?>
