e4d56f146791497f585c13139dc6e5858e51b787
Damian Dlugosch <damian.dlugosch@pelicantravel.local>
2019-11-07 08:50:44 +0100

    Add last question with redirection to deals

---
+++ otazka5_leto_burger.php
@@ -0,0 +1,38 @@
+<?php include "head.html" ?>
+<?php include "header.html" ?>
+<main>
+    <div class="container">
+        <section class="form col-lg-12">
+            <div class="row">
+                <h1 class="col-lg-12 text-center py-3">No a priznaj sa, čo máš na nohách?</h1>
+            </div>
+            <div class="row form--links py-4">
+                <a href="https://www.pelikan.sk/sk/multicity/sydney-vanuatu-fidzi" target="_blank" onclick="window.location.href='end.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/lodicky.png" alt="Lodičky">
+                    </figure>
+                    <h2 class="text-center">Lodičky</h2>
+                </a>
+                <a href="https://www.pelikan.sk/sk/multicity/sydney-vanuatu-fidzi" target="_blank" onclick="window.location.href='end.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/conversy.png" alt="Tenisky">
+                    </figure>
+                    <h2 class="text-center">Tenisky</h2>
+                </a>
+                <a href="https://www.pelikan.sk/sk/multicity/sydney-vanuatu-fidzi" target="_blank" onclick="window.location.href='end.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/sandale.png" alt="Sandále">
+                    </figure>
+                    <h2 class="text-center">Ponožky v&nbsp;sandáloch</h2>
+                </a>
+                <a href="https://www.pelikan.sk/sk/multicity/sydney-vanuatu-fidzi" target="_blank" onclick="window.location.href='end.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+                    <figure class="image-cropper">
+                        <img src="assets/images/timberlandy.png" alt="Turistické topánky">
+                    </figure>
+                    <h2 class="text-center">Turistické topánky</h2>
+                </a>
+            </div>
+        </section>
+    </div>
+</main>
+<?php include "footer.html"?>
