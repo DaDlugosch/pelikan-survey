01f42692f4f640c27ae92bcfd07664c4e78b73ff
Damian Dlugosch <damian.dlugosch@pelicantravel.local>
2019-11-07 10:17:06 +0100

    Change basic graphic of start page

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
             <div class="row">
                 <h1 class="col-lg-12 text-center py-3">Ako bude vyzerať tvoja vysnívaná multicity?</h1>
             </div>
-            <div class="row form--links py-4">
-                <p class="col-12">Si telom aj dušou cestovateľ a najradšej by si počas dvojtýždňovej dovolenky precestoval/a celý svet?</p>
-                <p class="col-12">Zlá správa je, že za 14 dní to asi nestihneš. Tá dobrá je, že náš kvíz ti ukáže kde začať.</p>
-                <a href="javascript:void(0)" onclick="window.location.href='otazka1.php';" class="col-lg-3 d-flex flex-column mb-4 mb-lg-0">
+            <div class="row form--links pb-4">
+                <p class="col-12 mb-0 text-center">
+                    Si telom aj dušou cestovateľ a najradšej by si počas dvojtýždňovej dovolenky precestoval/a celý svet?
+                    <br>
+                    Zlá správa je, že za 14 dní to asi nestihneš. Tá dobrá je, že náš kvíz ti ukáže kde začať.
+                </p>
+                <a href="javascript:void(0)" onclick="window.location.href='otazka1.php';" class="mx-auto my-4">
                     Idem nato
                 </a>
-                <p class="col-12">Odpovedz na 5 jednoduchých otázok, ktoré odhalia tvoje tajné cestovateľské kombinácie.</p>
+                <p class="col-12 text-center">Odpovedz na 5 jednoduchých otázok, ktoré odhalia tvoje tajné cestovateľské kombinácie.</p>
             </div>
         </section>
     </div>
